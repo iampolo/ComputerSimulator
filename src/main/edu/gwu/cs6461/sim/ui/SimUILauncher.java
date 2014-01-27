9bb03755db058cb85e1c405b20926b0219e8c053
marcoyeung <marcoyeung@macopolo-PC.home>
2014-01-26 18:50:25 -0800

    add logging

--- src/main/edu/gwu/cs6461/sim/ui/SimUILauncher.java
+++ src/main/edu/gwu/cs6461/sim/ui/SimUILauncher.java
@@ -1,17 +1,29 @@
 package edu.gwu.cs6461.sim.ui;
 
+import java.awt.Panel;
+
+import org.apache.log4j.Logger;
+
 
 /*
  * 
  *  
  */
 public class SimUILauncher {
+	private final static Logger logger = Logger.getLogger(SimUILauncher.class);
 
 	
 	
-	
+	private Panel createControlPanel(){
+		
+		logger.debug("     .......... test");
+		
+		
+		return new Panel();
+	}
 	
 	
 	public static void main(String[] args) {
+		new SimUILauncher().createControlPanel();
 	}
 }
