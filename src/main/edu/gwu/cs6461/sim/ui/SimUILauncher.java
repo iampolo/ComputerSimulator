01eebbd55bb6d6a86a155c65565f0d3c2713b1f0
marcoyeung <marcoyeung@macopolo-PC.home>
2014-01-26 21:37:04 -0800

    new items

--- src/main/edu/gwu/cs6461/sim/ui/SimUILauncher.java
+++ src/main/edu/gwu/cs6461/sim/ui/SimUILauncher.java
@@ -2,6 +2,8 @@ package edu.gwu.cs6461.sim.ui;
 
 import java.awt.Panel;
 
+import javax.swing.JFrame;
+
 import org.apache.log4j.Logger;
 
 
@@ -15,6 +17,12 @@ public class SimUILauncher {
 	
 	
 	private Panel createControlPanel(){
+
+		MainSimFrame  mFrame = new MainSimFrame();
+		
+		mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mFrame.setSize(500,400);
+		mFrame.setVisible(true);
 		
 		logger.debug("     .......... test");
 		
@@ -25,5 +33,8 @@ public class SimUILauncher {
 	
 	public static void main(String[] args) {
 		new SimUILauncher().createControlPanel();
+		
+		
+		
 	}
 }
